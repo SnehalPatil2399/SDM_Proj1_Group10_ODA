be057672edfb7ba8ce78a1ac004bf3a1fb52b9ea
SnehalPatil2399 <131233764+SnehalPatil2399@users.noreply.github.com>
2023-04-21 15:07:18 +0530

    Update db_ODA.sql

--- db_ODA.sql
+++ db_ODA.sql
@@ -1,14 +1,17 @@
 create table Patient (
  pid int primary key,
  pname varchar(20) not null,
- pmno int not null,
- pdesc varchar(80) 
+ pmno varchar(10) not null unique,
+ pemail varchar(30) not null unique,
+ ppass varchar(30) not null	
  );
 	
 create table Doctor (
  did int primary key,
  dname varchar(20) not null,
- demail varchar(20)  not null,   
+ demail varchar(30)  not null unique,   
+ dpass varchar(30) not null,
+ dmno varchar(10) not null unique,	
  specialization varchar(20) not null
  );
 	
@@ -17,19 +20,19 @@ create table Appointment (
  Aptid int primary key,
  pid int,
  did int,
+ testid int,
  atpdate date,
  atptime int,
  constraint fkk_pid foreign key(pid) references Patient(pid),
- constraint fk_did foreign key(did) references Doctor(did)
+ constraint fk_did foreign key(did) references Doctor(did),
+constarint fkkk_testid foreign key(testid) references Test(testid)
  );
 	
 	
 	
 create table Test (
  testid int primary key,
- pid int,
- tname varchar(20) not null,
- constraint fk_pid foreign key(pid) references Patient(pid)
+ tname varchar(80) not null,
  );
 	
 	
